c32e83c57c52b9eede5e1c03e207985033aefef4
Ruslan <32416160+GuruFront@users.noreply.github.com>
2017-11-02 17:06:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// task 1
+/*for (var i = 1; i <= 100; i++) {
+	console.log(i);
+	}*/
+
+
+
+// task 2
+// 	var n = prompt();
+// 	var a, b;
+// 	a = 1;
+
+// for (var i = 1; i <= n; i++) {
+// 	a = a * i;
+// }
+
+//  alert(a);
+
+
+// task 3
+ var a = prompt();
+ var x = a;
+ var b;
+	for ( b = 0;  x > 1; b++) {
+		if ( x % 2 == 0) {
+			x= x / 2;
+		}
+		else{
+			x = (x * 3) +1;
+		}
+	};
+console.log("Шагов = " + b);
+
+
+
+
+
+
+
+
